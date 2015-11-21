9ac3945c33fa8d4045a2ee70e0f1bb8978bea8bd
yt1109 <yt1109@ic.ac.uk>
2015-11-21 17:47:33 +0000

    changed testANN to give predictions choosing max output as positive

--- testANN.m
+++ testANN.m
@@ -4,21 +4,7 @@ net = a neural network, x2 = examples x2
 produces a vector of label predictions
 %}
 
-    predictions = zeros(size(x2, 1));
-    %steps through each row of examples, and predict a label
-    for i = 1: size(x2, 1)
-        t = sim(net, x2(1, :));
-        predictions(i) = NNout2labels(t);
-    end
+   Y = sim(net, x2);  
+   predictions = NNout2labels(Y);
 end
 
-function prediction = NNout2labels(t)
-% t is the output of a feed-forwarded Neural Network 
-% predictions is the corresponding output in the format that 1 = anger, 2 = disgust etc.
-	prediction = 0;
-	for i = 1: size(t, 2)
-        if (t(i) == 1) 
-            prediction = i;
-        end
-	end
-end
