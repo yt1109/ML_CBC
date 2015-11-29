ef4f5ecfd3475b2cbf0d327aaffdc210e4e7fb08
unknown <yt1109@ic.ac.uk>
2015-11-29 10:44:25 +0000

    fixed trainNetwork

--- trainNetwork.m
+++ trainNetwork.m
@@ -11,22 +11,24 @@ filename = strcat(trainingFcn, '_Network');
 %returns optimal parameters over all folds 
 OptimalParameters =  CrossValidateNN(x2, y2, kFold, trainingFcn);
 
-net = SetupNet(trainingFcn, OptimalParameters.layers, OptimalParameters.npl, x2, y2, (1:size(x2,2)), [], []);
-net = ConfigureTrainingParams(net, OptimalParameters, trainingFcn);
 
 
-net.trainParam.showWindow = true;
+
+
 
 bestMatrix = zeros(nClasses);
 bestCRate = 0;
-bestNet = net;
+
 for i=1:100
+    net = SetupNet(trainingFcn, OptimalParameters.layers, OptimalParameters.npl, x2, y2, (1:size(x2,2)), [], []);
+    net = ConfigureTrainingParams(net, OptimalParameters, trainingFcn);
     trainedNet = train(net, x2, y2);
     matrix = ConfusionMatrix([cleanData.y, testANN(trainedNet, x2)], nClasses);
     classificationRate = sum(diag(matrix)) / sum(sum(matrix));
     if classificationRate > bestCRate
         bestMatrix = matrix;
         bestNet = trainedNet;
+        bestCRate = classificationRate;
     end
 end
 
